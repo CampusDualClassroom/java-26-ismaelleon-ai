6d7673b40550cb5b189af511009cd00f640cc85e
Ismael León Cal <ileoncal@gmail.com>
2024-11-03 22:08:23 +0100

    Ejercicio 26 IP

--- src/main/java/com/campusdual/classroom/Contact.java
+++ src/main/java/com/campusdual/classroom/Contact.java
@@ -1,5 +1,51 @@
 package com.campusdual.classroom;
 
-public class Contact{
+import java.text.Normalizer;
 
+public class Contact implements ICallActions{
+
+    private String name;
+    private String surname;
+    private String phoneNumber;
+    private String code;
+
+
+    public Contact(String name, String surname, String phoneNumber, String code) {
+        this.name = name;
+        this.surname = surname;
+        this.phoneNumber = phoneNumber;
+        this.code = generateCode(name,surname);
+    }
+
+    private String generateCode(String firstName, String lastName) {
+        String normalizedLastName = Normalizer.normalize(lastName, Normalizer.Form.NFD)
+                .replaceAll("[^\\p{ASCII}]", "").toLowerCase().replace(" ", "");
+
+        if (!lastName.contains(" ")) {
+            return firstName.charAt(0) + normalizedLastName;
+        }
+
+        String[] lastNames = lastName.toLowerCase().split(" ");
+        String code = firstName.substring(0, 1) + lastNames[0].charAt(0) + lastNames[1];
+        return code;
+    }
+
+    public String getCode(){
+        return code;
+    }
+
+    @Override
+    public void callMyNumber() {
+
+    }
+
+    @Override
+    public void callOtherNumber(String number) {
+
+    }
+
+    @Override
+    public void showContactDetails() {
+
+    }
 }
--- src/main/java/com/campusdual/classroom/Phonebook.java
+++ src/main/java/com/campusdual/classroom/Phonebook.java
@@ -1,5 +1,57 @@
-package com.campusdual.classroom;
+    package com.campusdual.classroom;
 
-public class Phonebook {
+    import java.util.HashMap;
+    import java.util.Map;
+    import java.util.Scanner;
 
-}
+    public class Phonebook {
+
+        private static Map<String,Contact> createContactMap() {
+            Map<String, Contact> contactMap = new HashMap<>();
+            return contactMap;
+        }
+
+        public void addContact (){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Ingrese nombre: ");
+            String name = scanner.nextLine();
+            System.out.println("Ingrese apellidos: ");
+            String surname = scanner.nextLine();
+            System.out.println("Ingrese numero telefonico: ");
+            String phoneNumber = scanner.nextLine();
+
+            Contact contact = new Contact(name, surname, phoneNumber);
+            createContactMap().put()// no entiendo porque me lo almacena como un metodo
+            System.out.println("Contacto añadido");
+
+        }
+
+        public void mainMenu() {
+
+            Scanner scanner = new Scanner(System.in);
+            int num;
+            do {
+                System.out.println("\nMenú de acciones: ");
+                System.out.println("1. Añadir un contacto");
+                System.out.println("2. Mostrar contactos");
+                System.out.println("3. Seleccionar contacto");
+                System.out.println("4. Eliminar contacto");
+                System.out.println("5. Salir del menu");
+                num = scanner.nextInt();
+
+                switch (num) {
+                    case 1 =
+                        ;
+                    case 2 =
+                        ;
+                    case 3 =;
+                    case 4 =;
+                }
+
+            } while (num != 5);
+
+        }
+
+
+
+    }
